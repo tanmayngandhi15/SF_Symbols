5933391f5289c1fe8f38fd1b8eb9a94d19beb9a8
Tanmay <tanmayngandhi1996@gmail.com>
2023-12-09 14:20:19 +0530

    Initial Commit

---
+++ demo_SwiftUI_1/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  demo_SwiftUI_1
+//
+//  Created by Samir Raut on 02/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var activities = ["archery", "baseball", "basketball", "bowling", "boxing", "cricket", "curling", "fencing", "golf", "hiking", "lacrosse", "rugby", "squash"]
+    
+    let systemColors: [Color] = [.black, .red, .blue, .green, .orange, .purple, .pink]
+    @State var selected = "archery"
+    @State var randomSystemColor = Color.black
+    
+    @State var id = 0
+    
+    
+    var body: some View {
+
+        VStack {
+
+            Circle()
+                .fill(randomSystemColor)
+                .overlay(
+
+                    VStack {
+                        
+                        Image(systemName: "figure.\(selected)")
+                            .font(.system(size: 144))
+                            .foregroundStyle(.yellow)
+                        
+                        Text(selected.uppercased())
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.white)
+                    }
+ 
+                )
+                .padding(.minimum(10, 10))
+                .transition(.slide)
+                .id(selected)
+
+            
+            Text("Color: \(randomSystemColor.description)")
+                .font(.system(size: 40))
+                .italic()
+                .underline()
+                .foregroundStyle(.brown)
+            
+            Button("Click") {
+
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "archery"
+
+                    randomSystemColor = systemColors.randomElement() ?? .black
+                    id = activities.firstIndex(of: selected)!
+  
+                }
+                
+            }
+            .padding()
+            .font(.system(size: 30))
+            .buttonStyle(.bordered)
+            .foregroundStyle(.mint)
+            .tint(.orange)
+            
+            
+            Spacer()
+        }
+        .padding()
+        
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo_SwiftUI_1/demo_SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_SwiftUI_1App.swift
+//  demo_SwiftUI_1
+//
+//  Created by Samir Raut on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
